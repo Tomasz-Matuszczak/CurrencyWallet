b1696f4809c036e01bd0f1fd82d71b2dabf0069f
tomMat <tomasz.matuszczak@gmail.com>
2025-06-23 17:31:31 +0200

    c2_master

--- CurrencyWallet_solution — kopia/CurrencyWallet/Program.cs
+++ CurrencyWallet_solution — kopia/CurrencyWallet/Program.cs
@@ -14,6 +14,7 @@ builder.Services.AddSingleton<IWalletServices, WalletServices>();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 
 builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
